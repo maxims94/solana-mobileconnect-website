2c19a1ef5aa7e6b399031030c6544af65da18902
Maxim Schmidt <maxim.schmidt@tum.de>
2023-05-05 00:15:14 +0200

    nft-demo

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode } from "react";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from '@solana/web3.js'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+
+    const wallets = [
+      //new QRCodeWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ]
+
+    const endpoint = web3.clusterApiUrl('devnet')
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets}>
+                <WalletModalProvider>
+                    { children }
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
+
+export default WalletContextProvider
--- pages/nft-demo/index.tsx
+++ pages/nft-demo/index.tsx
@@ -9,12 +9,18 @@ import EmailIcon from '@/public/email.svg'
 import NftDemoLogo from '@/public/nft-demo-logo.svg'
 import MobileConnectIcon from '@/public/mobileconnect-icon.svg'
 
+import WalletContextProvider from '@/components/WalletContextProvider'
+
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
 export default function NftDemo() {
   return (
     <>
       <Head>
         <title>Solana MobileConnect - NFT Demo</title>
       </Head>
+
+      <WalletContextProvider>
       <main>
 
         <div className="flex flex-col h-screen min-h-[800px]">
@@ -25,7 +31,7 @@ export default function NftDemo() {
                 <Image src={NftDemoLogo} alt="" className="mr-[20px]" />
                 <h1 className="text-4xl">NFT Demo</h1>
               </div>
-              <div>Connect</div>
+              <div><WalletMultiButton /></div>
             </div>
           </div>
 
@@ -37,7 +43,7 @@ export default function NftDemo() {
               <h1 className="text-4xl mb-[40px]">Step 1</h1>
               <div>
                 Connect with “Mobile Wallet”
-                <Image src={MobileConnectIcon} className="inline-block ml-[10px]" />
+                <Image src={MobileConnectIcon} className="inline-block ml-[10px]" alt="MobileConnect icon" />
               </div>
             </div>
           </div>
@@ -63,6 +69,152 @@ export default function NftDemo() {
         </div>
 
       </main>
+      </WalletContextProvider>
     </>
   )
 }
+
+
+/*
+  const [txSig, setTxSig] = useState('');
+  const [balanceStatus, setBalanceStatus] = useState("unknown");
+  const isTransacting = useRef<boolean>(false);
+
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction, wallet } = useWallet();
+
+  const onConnect = useCallback(async (publicKey: PublicKey) => {
+    console.log("Connected:", publicKey.toString())
+
+    try {
+
+      setBalanceStatus("loading...")
+      const balance = await connection.getBalance(publicKey)
+
+      if (balance >= 1 * LAMPORTS_PER_SOL) {
+
+        setBalanceStatus((balance / LAMPORTS_PER_SOL) + " SOL")
+
+      } else {
+        isTransacting.current = true;
+
+        setBalanceStatus("airdrop...")
+
+        console.log("Request airdrop of 1 SOL...")
+        const signature = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL);
+
+        console.log("Finalize transaction...")
+        await connection.confirmTransaction(signature, "finalized");
+
+        const balance = await connection.getBalance(publicKey)
+        setBalanceStatus((balance / LAMPORTS_PER_SOL) + " SOL")
+        
+        isTransacting.current = false;
+        
+        console.log("Finalized")
+      }
+    } catch (error: any) {
+      console.error(error)
+      setBalanceStatus("error")
+    }
+  }, [connection, setBalanceStatus])
+
+  const onDisconnect = useCallback(() => {
+    console.log("Wallet disconnected")
+    setBalanceStatus("unknown")
+    setTxSig('')
+  }, [setTxSig])
+
+  useEffect(() => {
+
+    if (wallet !== null) {
+      wallet.adapter.on('connect', onConnect)
+      wallet.adapter.on('disconnect', onDisconnect)
+
+      return () => {
+        wallet.adapter.off('connect', onConnect)
+        wallet.adapter.off('disconnect', onDisconnect)
+
+      }
+    }
+
+  }, [onConnect, onDisconnect, wallet])
+
+  const sendSol = useCallback((event: any) => {
+
+    event.preventDefault()
+
+    if (!connection || !publicKey) {
+      alert("Please connect your wallet")
+      return
+    }
+
+    if (isTransacting.current) {
+      alert("Please wait for the previous transaction to complete")
+      return
+    }
+
+    setTxSig('');
+    isTransacting.current = true;
+
+    (
+      async () => {
+        const receiver = event.target.receiver.value
+        const amount = event.target.amount.value
+
+        const tx = new Transaction()
+
+        const senderPubKey = publicKey
+        const receiverPubKey = new PublicKey(receiver)
+
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: senderPubKey,
+            toPubkey: receiverPubKey,
+            lamports: LAMPORTS_PER_SOL * Number(amount)
+          })
+        )
+
+        tx.add(
+          new TransactionInstruction({
+            programId: new PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
+            keys: [],
+            data: Buffer.from(uuid(), 'utf-8')
+          })
+        )
+
+        tx.feePayer = senderPubKey
+
+        try {
+
+          const latestBlockhash = await connection.getLatestBlockhash()
+          tx.recentBlockhash = latestBlockhash.blockhash
+
+          const sig = await sendTransaction(tx, connection)
+
+          console.log("transaction signature: ", sig)
+          setTxSig(sig)
+
+          setBalanceStatus("loading...")
+          console.log("Finalize transaction...")
+          await connection.confirmTransaction(sig, "finalized")
+
+          console.log("Finalized")
+
+          const balance = await connection.getBalance(publicKey)
+
+          console.log("New balance:", balance / LAMPORTS_PER_SOL)
+
+          setBalanceStatus((balance / LAMPORTS_PER_SOL) + " SOL")
+
+        } catch (error: any) {
+          console.error(error)
+        } finally {
+          isTransacting.current = false;
+        }
+
+      }
+    )().catch(console.error)
+
+  }, [setTxSig, connection, publicKey, sendTransaction])
+*/
