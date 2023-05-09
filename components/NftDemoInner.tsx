dea5ce26241273ac3c2d8b62b78a82a9304b4159
Maxim Schmidt <maxim.schmidt@tum.de>
2023-05-09 17:29:15 +0200

    line height

--- components/NftDemoInner.tsx
+++ components/NftDemoInner.tsx
@@ -171,7 +171,7 @@ export default function NftDemoInner({ titleEvent }: { titleEvent: EventEmitter 
       <div className="flex flex-row justify-center pt-[100px] pb-[100px] grow">
         <div className="flex flex-col items-center w-3/4 max-w-screen-xl">
           <h1 className="text-5xl mb-[40px] font-monda font-bold">Step 1</h1>
-          <div className="text-3xl">
+          <div className="text-3xl leading-loose">
             <p>Connect your mobile <b>Solflare</b> or <b>Glow</b> wallet using <Image src={MobileConnectIcon} className="inline-block ml-2" alt="MobileConnect icon" height={40} /> <b>MobileConnect</b></p>
           </div>
         </div>
